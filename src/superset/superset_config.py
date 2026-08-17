67384a78a75ec349711e9b9879de32c39c6727ea
HuaTanSang <huatansang2004@gmail.com>
2026-08-17 23:57:20 +0700

    feat: add superset

--- src/superset/superset_config.py
+++ src/superset/superset_config.py
@@ -1,8 +1,16 @@
 import os
+from urllib.parse import quote_plus
 
 SECRET_KEY = os.environ["SUPERSET_SECRET_KEY"]
 
-SQLALCHEMY_DATABASE_URI = os.environ["SUPERSET_DATABASE_URI"]
+postgres_user = quote_plus(os.environ["POSTGRES_USER"])
+postgres_password = quote_plus(os.environ["POSTGRES_PASSWORD"])
+postgres_database = os.environ["POSTGRES_DB"]
+
+SQLALCHEMY_DATABASE_URI = (
+    f"postgresql+psycopg2://{postgres_user}:"
+    f"{postgres_password}@superset-db:5432/{postgres_database}"
+)
 
 WTF_CSRF_ENABLED = True
 
@@ -11,3 +19,6 @@ ROW_LIMIT = 5000
 SUPERSET_WEBSERVER_PORT = 8088
 
 ENABLE_PROXY_FIX = False
+
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SAMESITE = "Lax"
